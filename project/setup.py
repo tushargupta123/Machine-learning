e4173ed1aaa6b2506f4c826afc1654639af3a279
tushargupta123 <tushargupta2k3@gmail.com>
2023-09-11 19:15:10 +0530

    project completed

--- project/setup.py
+++ project/setup.py
@@ -1,21 +1,26 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 from typing import List
-HYPHEN_E_DOT = '-e .'
 
-def get_requirements(file_path:str)->List[str]:
+HYPEN_E_DOT='-e .'
+
+
+def get_requiremet(file_path:str)->List[str]:
     requirements = []
     with open(file_path) as f:
         requirements=f.readlines()
-        requirements=[req.replace("/n","") for req in requirements]
-        if HYPHEN_E_DOT in requirements:
-            requirements.remove(HYPHEN_E_DOT)
-        return requirements
+        requirements=[req.replace("\n","")for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+        
+    return requirements
+
 
 setup(
     name="src",
     version="0.0.1",
     author="tushar",
     author_email="tushargupta2k3@gmail.com",
-    install_requires=get_requirements("requirements.txt"),
+    install_requires=get_requiremet("requirements.txt"),
     packages=find_packages()
 )
